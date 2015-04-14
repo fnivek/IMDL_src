32cd33080628d1734fa3a502fc0fed0b65f86982
Kevin French <fnivek@gmail.com>
2015-04-14 17:15:42 -0400

    Added tf

--- percept_generators/scripts/encoder_fwd_kinematics.py
+++ percept_generators/scripts/encoder_fwd_kinematics.py
@@ -7,57 +7,82 @@
 import rospy
 import numpy as np
 from std_msgs.msg import Float64MultiArray
+import tf
 
-# Distance between centers of wheels
-wheel_base = 0.3048 # 0.3048 m = 12 in
-wheel_radius = 0.06
+class node:
+	def __init__(self):
+		self.motor_vel_sub = rospy.Subscriber('motor_vels', Float64MultiArray, self.vels_cb)
+		self.listener = tf.TransformListener()
+		self.last_time = 0
+		self.last_R = 0
+		self.last_w = 0
+		self.wheel_base = 0.3048 # 0.3048 m = 12 in
+		self.wheel_radius = 0.06
 
-last_time = 0
-last_w = 0
-last_R = 0
+	# Use avg of last R and last w with current
+	# multiply by the time between cb's
+	def vels_cb(self, msg):
 
-# Use avg of last R and last w with current
-# multiply by the time between cb's
-def vels_cb(msg):
-	global last_time
-	global last_R
-	global last_w
-	# Time step
-	now = rospy.get_time()
-	time_step = now - last_time
-	last_time = now
+		# Time step
+		now = rospy.get_time()
+		time_step = now - self.last_time
+		self.last_time = now
 
-	Vl = msg.data[0]
-	Vr = msg.data[1]
+		Vl = msg.data[0]
+		Vr = msg.data[1]
 
-	if(Vl == Vr):
-		print 'Forward Motion: %f' % (Vl * time_step * wheel_radius) # w * t * r = arc length
-		return
-	# Instantanious values
-	R = (wheel_base * (Vl + Vr)) / (2 * (Vr - Vl))
-	w = (Vr - Vl) / wheel_base
+		dx = 0
+		dy = 0
+		dtheta = 0
 
-	R_avg = (R + last_R) / 2
-	w_avg = (w + last_w) / 2
+		if(Vl == Vr):
+			dx = Vl * time_step * self.wheel_radius # w * t * r = arc length
+		else:
+			# Instantanious values
+			R = (self.wheel_base * (Vl + Vr)) / (2 * (Vr - Vl))
+			w = (Vr - Vl) / self.wheel_base
 
-	last_R = R
-	last_w = w
+			R_avg = (R + self.last_R) / 2
+			w_avg = (w + self.last_w) / 2
 
-	# Kinematic equations assuming no slip
-	print time_step
-	dtheta = time_step * w_avg
-	dx = R_avg * np.sin(dtheta)
-	dy = R_avg * (1 - np.cos(dtheta))
+			self.last_R = R
+			self.last_w = w
 
-	print (dx, dy, dtheta)
+			# Kinematic equations assuming no slip
+			dtheta = time_step * w_avg
+			dx = R_avg * np.sin(dtheta)
+			dy = R_avg * (1 - np.cos(dtheta))
 
+		trans = [0,0,0]	#x y z
+		rot = [0,0,0,1] # x y z w
+
+		try:
+			(trans, rot) = self.listener.lookupTransform('/world', '/base_link', rospy.Time(0))
+		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+			rospy.logerr('TF error: %s' % e)
+			# TODO: make sure we don't reset our position on an error
+
+		# Transform cords
+		trash, trash, theta = tf.transformations.euler_from_quaternion(rot)
+		mag = np.sqrt(dx*dx + dy*dy)
+		dx = np.cos(theta) * mag
+		dy = np.sin(theta) * mag
+		rot = tf.transformations.quaternion_multiply(rot, tf.transformations.quaternion_from_euler(0, 0, dtheta))		
+		
+		# Set up a tf brodcaster
+		br = tf.TransformBroadcaster()
+		br.sendTransform((trans[0] + dx, trans[1] + dy, 0),
+						 rot,
+						 rospy.Time.now(),
+						 'base_link',
+						 'world')
 
 def main():
 	# Ros initilization
 	#Init ros
 	rospy.init_node('encoder_fwd_kinematics', anonymous=False)
 
-	rospy.Subscriber("motor_vels", Float64MultiArray, vels_cb)
+	this_node = node()
 
 	rospy.spin()
 
