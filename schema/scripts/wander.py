c9b6dd49217f02a6550f929ff7d627e12b545867
Kevin French <fnivek@gmail.com>
2015-04-21 22:38:57 -0400

    Use rostime

--- schema/scripts/wander.py
+++ schema/scripts/wander.py
@@ -16,7 +16,7 @@ class schema(schema_base):
 		mag = np.random.random_sample() * self.max_pfield
 		angle = np.random.random_sample() * np.pi - np.pi / 2
 		field = pfield()
-		field.header.stamp = rospy.get_time()
+		field.header.stamp = rospy.get_rostime()
 		field.header.frame_id = 'base_link'
 		field.vector.x = np.cos(angle) * mag
 		field.vector.y = np.sin(angle) * mag
